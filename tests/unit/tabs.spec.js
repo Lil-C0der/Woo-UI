ac7c0f73338dcc42031115aea0f2bbc0f71fd250
Lil-C0der <781172643@qq.com>
2020-05-28 14:35:31 +0800

    test: 添加 Tabs 组件 change 事件和 tab-click 事件的测试用例

--- tests/unit/tabs.spec.js
+++ tests/unit/tabs.spec.js
@@ -1,5 +1,7 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
 import { mount } from "@vue/test-utils";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
 
 import Tabs from "@/Tabs/Tabs.vue";
 import TabsHead from "@/Tabs/TabsHead.vue";
@@ -9,8 +11,16 @@ import TabsBody from "@/Tabs/TabsBody.vue";
 
 import Vue from "vue";
 
+chai.use(sinonChai);
+
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+
+Vue.component("woo-tabs", Tabs);
+Vue.component("woo-tabs-head", TabsHead);
+Vue.component("woo-tabs-item", TabsItem);
+Vue.component("woo-tabs-pane", TabsPane);
+Vue.component("woo-tabs-body", TabsBody);
 
 describe("Tabs组件", () => {
   it("存在", () => {
@@ -26,14 +36,7 @@ describe("Tabs组件", () => {
     }).to.throw();
   });
 
-  // TODO 用 vue-test-utils 优化
   it("可以设置 activeName ", () => {
-    Vue.component("woo-tabs", Tabs);
-    Vue.component("woo-tabs-head", TabsHead);
-    Vue.component("woo-tabs-item", TabsItem);
-    Vue.component("woo-tabs-pane", TabsPane);
-    Vue.component("woo-tabs-body", TabsBody);
-
     const wrapper = mount(Tabs, {
       attachToDocument: true,
       propsData: {
@@ -45,7 +48,6 @@ describe("Tabs组件", () => {
               <woo-tabs-item name="1st">1</woo-tabs-item>
               <woo-tabs-item name="2nd">2</woo-tabs-item>
               <woo-tabs-item name="3rd">3</woo-tabs-item>
-              <woo-tabs-item name="4th">4</woo-tabs-item>
             </woo-tabs-head>
             <woo-tabs-body>
               <woo-tabs-pane name="1st">A</woo-tabs-pane>
@@ -55,12 +57,76 @@ describe("Tabs组件", () => {
       },
     });
     const vm = wrapper.vm;
-
+    const tabsItemWrapper = wrapper.find('[data-name="2nd"]', ".woo-tabs-item");
     vm.$nextTick().then(() => {
-      const tabsItemList = vm.$el.querySelectorAll(".woo-tabs-item");
-      expect(tabsItemList[1].classList.contains("item-active")).to.eq(true);
+      expect(tabsItemWrapper.classes().includes("item-active")).to.eq(true);
+      wrapper.destroy();
     });
-    wrapper.destroy();
   });
-  // TODO isVertical
+
+  it("可以触发 tab-click 事件 ", () => {
+    const callback = sinon.fake();
+    const wrapper = mount(Tabs, {
+      attachToDocument: true,
+      slots: {
+        default: `
+            <woo-tabs-head>
+              <woo-tabs-item name="1st">1</woo-tabs-item>
+              <woo-tabs-item name="2nd">2</woo-tabs-item>
+              <woo-tabs-item name="3rd">3</woo-tabs-item>
+            </woo-tabs-head>
+            <woo-tabs-body>
+              <woo-tabs-pane name="1st">A</woo-tabs-pane>
+              <woo-tabs-pane name="2nd">B</woo-tabs-pane>
+              <woo-tabs-pane name="3rd">C</woo-tabs-pane>
+            </woo-tabs-body>`,
+      },
+      listeners: {
+        "tab-click": callback,
+      },
+    });
+    const vm = wrapper.vm;
+    const tabsItemWrapper = wrapper.find('[data-name="3rd"]', ".woo-tabs-item");
+    vm.$nextTick()
+      .then(() => {
+        tabsItemWrapper.trigger("click");
+      })
+      .then(() => {
+        expect(callback).to.have.been.calledWith("3rd");
+        wrapper.destroy();
+      });
+  });
+
+  it("可以触发 change 事件 ", () => {
+    const callback = sinon.fake();
+    const wrapper = mount(Tabs, {
+      attachToDocument: true,
+      slots: {
+        default: `
+            <woo-tabs-head>
+              <woo-tabs-item name="1st">1</woo-tabs-item>
+              <woo-tabs-item name="2nd">2</woo-tabs-item>
+              <woo-tabs-item name="3rd">3</woo-tabs-item>
+            </woo-tabs-head>
+            <woo-tabs-body>
+              <woo-tabs-pane name="1st">A</woo-tabs-pane>
+              <woo-tabs-pane name="2nd">B</woo-tabs-pane>
+              <woo-tabs-pane name="3rd">C</woo-tabs-pane>
+            </woo-tabs-body>`,
+      },
+      listeners: {
+        change: callback,
+      },
+    });
+    const vm = wrapper.vm;
+    const tabsItemWrapper = wrapper.find('[data-name="3rd"]', ".woo-tabs-item");
+    vm.$nextTick()
+      .then(() => {
+        tabsItemWrapper.trigger("click");
+      })
+      .then(() => {
+        expect(callback).to.have.been.calledWith("3rd");
+        wrapper.destroy();
+      });
+  });
 });
