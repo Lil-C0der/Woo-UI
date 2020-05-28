81aab26324403886d2f34990227682f11dcf3da3
Lil-C0der <781172643@qq.com>
2020-05-28 18:52:38 +0800

    test: 完成 Sider 组件单元测试

---
+++ tests/unit/sider.spec.js
@@ -0,0 +1,80 @@
+import chai, { expect } from "chai";
+import { mount } from "@vue/test-utils";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+
+import Sider from "@/Layout/Sider.vue";
+
+chai.use(sinonChai);
+
+describe("Sider 组件", () => {
+  it("存在", () => {
+    expect(Sider).to.exist;
+  });
+
+  it("可以设置 width", () => {
+    const wrapper = mount(Sider, {
+      attachToDocument: true,
+      propsData: {
+        width: "400px",
+      },
+    });
+    const vm = wrapper.vm;
+    const { width } = window.getComputedStyle(vm.$el);
+    expect(width).to.eq("400px");
+    wrapper.destroy();
+  });
+
+  it("可以设置 collapsible", () => {
+    const wrapper = mount(Sider, {
+      attachToDocument: true,
+      propsData: {
+        collapsible: true,
+      },
+    });
+    const vm = wrapper.vm;
+    const triggerWrapper = wrapper.find(".woo-sider-trigger");
+    expect(triggerWrapper.exists()).to.eq(true);
+    wrapper.destroy();
+  });
+
+  it("可以设置 collapsed", () => {
+    const wrapper = mount(Sider, {
+      attachToDocument: true,
+      propsData: {
+        collapsed: true,
+      },
+    });
+    const vm = wrapper.vm;
+    expect(wrapper.classes()).to.includes("sider-collapsed");
+    wrapper.destroy();
+  });
+
+  it("可以设置 collapsedWidth", () => {
+    const wrapper = mount(Sider, {
+      attachToDocument: true,
+      propsData: {
+        collapsed: true,
+        collapsedWidth: "100px",
+      },
+    });
+    const vm = wrapper.vm;
+    const { width } = window.getComputedStyle(vm.$el);
+    expect(width).to.eq("100px");
+    wrapper.destroy();
+  });
+
+  it("可以设置 reverseArrow", () => {
+    const wrapper = mount(Sider, {
+      attachToDocument: true,
+      propsData: {
+        collapsible: true,
+        reverseArrow: true,
+      },
+    });
+    const vm = wrapper.vm;
+    const iconWrapper = wrapper.find(".woo-icon");
+    expect(iconWrapper.classes()).to.includes("sider-icon-reverse");
+    wrapper.destroy();
+  });
+});
