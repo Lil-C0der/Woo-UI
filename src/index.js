0280cf4adf358844a7d1d29afb47d569b2d0bd70
Lil-C0der <781172643@qq.com>
2020-06-29 19:04:58 +0800

    chore: 修改组件导出命名

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-export { default as Button } from "./Button/Button.vue";
-export { default as ButtonGroup } from "./Button/ButtonGroup.vue";
+export { default as WooButton } from "./Button/Button.vue";
+export { default as WooButtonGroup } from "./Button/ButtonGroup.vue";
 
-export { default as Cascader } from "./Cascader/Cascader.vue";
-export { default as CascaderMenu } from "./Cascader/CascaderMenu.vue";
+export { default as WooCascader } from "./Cascader/Cascader.vue";
+export { default as WooCascaderMenu } from "./Cascader/CascaderMenu.vue";
 
-export { default as Collapse } from "./Collapse/Collapse.vue";
-export { default as CollapseItem } from "./Collapse/CollapseItem.vue";
+export { default as WooCollapse } from "./Collapse/Collapse.vue";
+export { default as WooCollapseItem } from "./Collapse/CollapseItem.vue";
 
-export { default as Col } from "./Grid/Col.vue";
-export { default as Row } from "./Grid/Row.vue";
+export { default as WooCol } from "./Grid/Col.vue";
+export { default as WooRow } from "./Grid/Row.vue";
 
-export { default as Content } from "./Layout/Content.vue";
-export { default as Footer } from "./Layout/Footer.vue";
-export { default as Header } from "./Layout/Header.vue";
-export { default as Layout } from "./Layout/Layout.vue";
-export { default as Sider } from "./Layout/Sider.vue";
+export { default as WooContent } from "./Layout/Content.vue";
+export { default as WooFooter } from "./Layout/Footer.vue";
+export { default as WooHeader } from "./Layout/Header.vue";
+export { default as WooLayout } from "./Layout/Layout.vue";
+export { default as WooSider } from "./Layout/Sider.vue";
 
-export { default as Menu } from "./Menu/Menu.vue";
-export { default as MenuItem } from "./Menu/MenuItem.vue";
-export { default as Submenu } from "./Menu/Submenu.vue";
+export { default as WooMenu } from "./Menu/Menu.vue";
+export { default as WooMenuItem } from "./Menu/MenuItem.vue";
+export { default as WooSubmenu } from "./Menu/Submenu.vue";
 
-export { default as Slide } from "./Slide/Slide.vue";
-export { default as SlideItem } from "./Slide/SlideItem.vue";
+export { default as WooSlide } from "./Slide/Slide.vue";
+export { default as WooSlideItem } from "./Slide/SlideItem.vue";
 
-export { default as Tabs } from "./Tabs/Tabs.vue";
-export { default as TabsBody } from "./Tabs/TabsBody.vue";
-export { default as TabsHead } from "./Tabs/TabsHead.vue";
-export { default as TabsItem } from "./Tabs/TabsItem.vue";
-export { default as TabsPane } from "./Tabs/TabsPane.vue";
+export { default as WooTabs } from "./Tabs/Tabs.vue";
+export { default as WooTabsBody } from "./Tabs/TabsBody.vue";
+export { default as WooTabsHead } from "./Tabs/TabsHead.vue";
+export { default as WooTabsItem } from "./Tabs/TabsItem.vue";
+export { default as WooTabsPane } from "./Tabs/TabsPane.vue";
 
-export { default as Icon } from "./Icon.vue";
+export { default as WooIcon } from "./Icon.vue";
 
-export { default as Input } from "./Input.vue";
+export { default as WooInput } from "./Input.vue";
 
-export { default as Popover } from "./Popover.vue";
+export { default as WooPopover } from "./Popover.vue";
 
-export { default as Toast } from "./Toast.vue";
+export { default as WooToast } from "./Toast.vue";
 
-export { default as ClickOutside } from "./click-outside";
-export { default as Plugin } from "./plugin";
+export { default as WooClickOutside } from "./click-outside";
+export { default as WooPlugin } from "./plugin";
